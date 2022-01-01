0465edc32f95f0d4808ae2ad020a178e7d5642e5
Lodney Santos <a21505293@alunos.ulht.pt>
2022-01-01 23:44:08 +0000

    botão requisitar --> book_details_screen

--- lib/prateleiras/prateleira_amarela/ecra_de_detalhes/book_details.dart
+++ lib/prateleiras/prateleira_amarela/ecra_de_detalhes/book_details.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -5,6 +7,30 @@ class BookDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // ---> BOTÃO REQUISITAR <---
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+        height: 49,
+        color: Colors.transparent,
+        child: FlatButton(
+          color: const Color.fromRGBO(18, 157, 158, 1),
+          onPressed: () {},
+          child: Text(
+            'Requisitar',
+            style: GoogleFonts.catamaran(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+
+      // ---> TERMINA EDIÇÃO BOTÃO REQUISITAR <---
+
       body: SafeArea(
         child: Container(
           child: CustomScrollView(
@@ -67,12 +93,12 @@ class BookDetailScreen extends StatelessWidget {
                         style: GoogleFonts.catamaran(
                           fontSize: 14,
                           fontWeight: FontWeight.w400,
-                          color: Colors.grey,
+                          color: Colors.grey.shade600,
                         ),
                       ),
                     ),
                     Container(
-                      height: 28,
+                      height: 45,
                       margin: const EdgeInsets.only(top: 23, bottom: 36),
                       padding: const EdgeInsets.only(left: 25),
                       child: DefaultTabController(
@@ -84,14 +110,34 @@ class BookDetailScreen extends StatelessWidget {
                           labelColor: Colors.black,
                           unselectedLabelColor: Colors.grey,
                           labelStyle: GoogleFonts.catamaran(
-                            fontSize: 14,
+                            fontSize: 20,
                             fontWeight: FontWeight.w700,
                           ),
                           unselectedLabelStyle: GoogleFonts.catamaran(
                               fontSize: 14, fontWeight: FontWeight.w600),
-                          indicator: RoundedRectangleTabIndicator(),
-                          tabs: [],
+                          tabs: [
+                            Tab(
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 23),
+                                child: const Text('Descrição'),
+                              ),
+                            ),
+                          ],
                         ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 25, right: 25, bottom: 25),
+                      child: Text(
+                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey,
+                          letterSpacing: 1.5,
+                          height: 2,
+                        ),
+                        textAlign: TextAlign.justify,
                       ),
                     ),
                   ],
