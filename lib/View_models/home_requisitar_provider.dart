8c1181905cc0d7b62b858da80c5f5d37db9ad9f6
Lodney Santos <a21505293@alunos.ulht.pt>
2022-03-10 20:26:04 +0000

    ....

--- lib/View_models/home_requisitar_provider.dart
+++ lib/View_models/home_requisitar_provider.dart
@@ -2,32 +2,31 @@ import 'package:camarate_school_library/Models/livro.dart';
 import 'package:flutter/material.dart';
 
 class HomeRequisitarProvider extends ChangeNotifier {
-  /// Variavel privada [livro], para chamar e guardar o livro por id.
+  //** Variável privada [livro], para chamar e guardar o livro por id. */
   late GerarLivro _livro;
 
-  /// Lista privada para armazenar os ids de cada livro.
+  //** Lista privada para armazenar os ids de cada livro */.
   final List<int> _idsDoslivros = [];
 
-  /// Com esta [_livro] depois consigo obter os livros por ids numéricos.
+  //** Com esta variável [_livro] obtenho os livros por ids */
   GerarLivro get livro => _livro;
 
-  /// Lista para guardar os livros por id e transformar em lista.
+  //** Lista para guardar os livros por id na lista de _idsDoslivros no formato de uma lista. */
   List<Livro> get livros =>
       _idsDoslivros.map((id) => _livro.getPorId(id)).toList();
 
-  /// Adicionar o [livro].
+  //** Adicionar o [livro requisitado]. */
   void addLivroRequisitado(Livro livro) {
     _idsDoslivros.add(livro.id);
-    // Esta linha diz ao [ModeloDoLivro] que ele deve reconstruir os widgets que
-    // dependem do --> notifyListeners.
+
+    /// Notifica, caso exista um livro que foi requisitado
     notifyListeners();
   }
 
   set livro(GerarLivro novoLivro) {
     _livro = novoLivro;
-    // Notifica, caso um livro forneça informações
-    // diferente do anterior. Por exemplo, a disponibilidade de um livro
-    // pode ter mudado.
+
+    /// Notifica, caso exista um novo livro
     notifyListeners();
   }
 }
