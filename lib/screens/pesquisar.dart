48cf063eb12a39583267621feca3a557ad437879
Lodney Santos <a21505293@alunos.ulht.pt>
2022-03-16 20:14:30 +0000

    area da pesquisa

--- lib/screens/pesquisar.dart
+++ lib/screens/pesquisar.dart
@@ -1,4 +1,6 @@
 // ignore: use_key_in_widget_constructors
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Pesquisar extends StatefulWidget {
@@ -16,12 +18,66 @@ class _PesquisarState extends State<Pesquisar> {
         title: const Text('Pesquisar'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: const [
-          Center(
-            child: Text('livros'),
-          ),
+          SizedBox(height: 30),
+          // Campo para fazer a pesquisa do livro
+          AreaDaPesquisa()
         ],
+      ),
+    );
+  }
+}
+
+//* Esta classe constrói a área para o utilizador escrever o que deseja pesquisar
+
+class AreaDaPesquisa extends StatelessWidget {
+  const AreaDaPesquisa({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(16, 0.0, 16, 0.0),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.black,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 17,
+            vertical: 8,
+          ),
+          child: Row(
+            children: [
+              const Icon(
+                CupertinoIcons.search,
+                color: Colors.blue,
+              ),
+              const Expanded(
+                child: CupertinoTextField(
+                  style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontSize: 16,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  cursorColor: Color.fromRGBO(0, 122, 255, 1),
+                  decoration: null,
+                ),
+              ),
+              GestureDetector(
+                child: const Icon(
+                  CupertinoIcons.clear_thick_circled,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
