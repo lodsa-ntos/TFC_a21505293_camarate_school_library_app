7e27307d3d55e904eecc5d61794ac2c0b359a985
Lodney Santos <a21505293@alunos.ulht.pt>
2022-04-20 14:58:47 +0100

    livro requisitado na BD ? true or false

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -258,74 +258,36 @@ class _FormatoLivroRequisitadoParaUtilizador extends StatelessWidget {
     return Consumer<LivroRequisitadoModel>(
       builder: (BuildContext context, LivroRequisitadoModel requisitadoModel,
           Widget? child) {
-        return Expanded(
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            shrinkWrap: true,
-            itemCount: requisitadoModel.livros.length,
-            itemBuilder: (context, index) {
-              final livro = requisitadoModel.livros.values.toList()[index];
+        final referenciaBD = FirebaseDatabase.instance.ref().child('livros');
+        final fazerLigacao = BaseDeDados();
 
-              return InkWell(
-                onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    //** Redireciona o utilizador para a página de detalhes do livro */
-                    builder: (context) => LivroDetalhado(
-                      livro: livro,
-                    ),
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 111.0,
-                      margin: const EdgeInsets.only(left: 16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 121.66,
-                            height: 165.5,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              image: DecorationImage(
-                                image: NetworkImage(livro.imagePath),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 12.0,
-                          ),
-                          Text(
-                            livro.titulo,
-                            style: const TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.w700,
-                              fontSize: 14.0,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5.0,
-                          ),
-                          Text(
-                            'Data de Entrega: ' + data,
-                            style: GoogleFonts.catamaran(
-                              textStyle: const TextStyle(
-                                fontSize: 13.0,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
+        return FutureBuilder(
+          future: fazerLigacao.carregarLivrosBD(referenciaBD), // async work
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: requisitadoModel.livros.length,
+                itemBuilder: (context, index) {
+                  final livro = requisitadoModel.livros.values.toList()[index];
+
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return const Center(child: CircularProgressIndicator());
+                    default:
+                      if (snapshot.hasError) {
+                        return Text('Erro: ${snapshot.error}');
+                      } else if (snapshot.data[index].isRequisitado == true) {
+                        return Text('tem livro requisitado');
+                      } else {
+                        return Text('nao tem nada aqui');
+                      }
+                  }
+                },
+              ),
+            );
+          },
         );
       },
     );
