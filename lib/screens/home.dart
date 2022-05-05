b41c4b6b19f86c879751c746dc28787d10bf2481
Lodney Santos <a21505293@alunos.ulht.pt>
2022-05-05 23:41:25 +0100

    ...

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -64,8 +64,8 @@ class _HomeState extends State<Home> {
         final referenciaBD = FirebaseDatabase.instance.ref().child('livros');
         final fazerLigacao = BaseDeDados();
 
-        return FutureBuilder(
-          future: fazerLigacao.carregarLivrosBD(referenciaBD), // async work
+        return StreamBuilder(
+          stream: FirebaseDatabase.instance.ref("livros").onValue, // async work
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.waiting:
@@ -85,80 +85,107 @@ class _HomeState extends State<Home> {
                 if (snapshot.hasError) {
                   return Text('Erro: ${snapshot.error}');
                 } else {
-                  return ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      //
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              //** Redireciona o utilizador para a página de detalhes do livro */
-                              builder: (context) => LivroDetalhado(
-                                livro: snapshot.data[index],
-                              ),
-                            ),
-                          );
-                        },
+                  if (snapshot.hasData &&
+                      !snapshot.hasError &&
+                      snapshot.data.snapshot.value != null) {
+                    List<dynamic> dadosBaseDeDados =
+                        jsonDecode(jsonEncode(snapshot.data.snapshot.value));
 
-                        //** Widgets que vão desenvolver formato dos livros a serem apresentados
-                        //** no página home */
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 2, vertical: 8),
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 122.0,
-                                margin: const EdgeInsets.only(left: 16.0),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: 121.66,
-                                      height: 190.5,
-                                      decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(8.0),
-                                        image: DecorationImage(
-                                          //** Capa */
-                                          image: NetworkImage(
-                                            snapshot.data[index].imagePath,
-                                          ),
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                    ),
+                    LivroModel listaDeLivros =
+                        LivroModel.fromJson(dadosBaseDeDados);
 
-                                    const SizedBox(height: 12.0),
+                    List<Livro> _livrosPrateleira = [];
 
-                                    //** Titulo */
-                                    Text(snapshot.data[index].titulo,
-                                        style: textTheme),
+                    _livrosPrateleira.addAll(listaDeLivros.livros);
 
-                                    const SizedBox(height: 5.0),
-
-                                    //** Autor */
-                                    Text(snapshot.data[index].autor),
-
-                                    const SizedBox(height: 5.0),
-
-                                    //** isRequisitado */
-                                    Text(snapshot.data[index].isRequisitado
-                                        .toString()),
-                                  ],
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _livrosPrateleira.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        //
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                //** Redireciona o utilizador para a página de detalhes do livro */
+                                builder: (context) => LivroDetalhado(
+                                  livro: _livrosPrateleira[index],
                                 ),
                               ),
-                            ],
+                            );
+                          },
+
+                          //** Widgets que vão desenvolver formato dos livros a serem apresentados
+                          //** no página home */
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 2, vertical: 8),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 122.0,
+                                  margin: const EdgeInsets.only(left: 16.0),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        width: 121.66,
+                                        height: 190.5,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8.0),
+                                          image: DecorationImage(
+                                            //** Capa */
+                                            image: NetworkImage(
+                                              _livrosPrateleira[index]
+                                                  .imagePath
+                                                  .toString(),
+                                            ),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+
+                                      const SizedBox(height: 12.0),
+
+                                      //** Titulo */
+                                      Text(
+                                        _livrosPrateleira[index]
+                                            .titulo
+                                            .toString(),
+                                        style: textTheme,
+                                      ),
+
+                                      const SizedBox(height: 5.0),
+
+                                      //** Autor */
+                                      Text(_livrosPrateleira[index]
+                                          .autor
+                                          .toString()),
+
+                                      const SizedBox(height: 5.0),
+
+                                      //** isRequisitado */
+                                      Text(
+                                        _livrosPrateleira[index]
+                                            .isRequisitado
+                                            .toString(),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                  );
+                        );
+                      },
+                    );
+                  }
                 }
+                return const Text("No data");
             }
           },
         );
@@ -273,8 +300,6 @@ class _HomeState extends State<Home> {
                             List<Livro> _livros = [];
 
                             _livros.addAll(listaDeLivros.livros);
-
-                            print(listaDeLivros.livros.length);
 
                             return Container(
                               margin: const EdgeInsets.symmetric(vertical: 8.0),
