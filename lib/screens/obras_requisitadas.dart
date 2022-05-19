495c90dcea9176507b58658f56b64f2788f09407
Lodney Santos <a21505293@alunos.ulht.pt>
2022-05-19 14:13:54 +0100

    ...

--- lib/screens/obras_requisitadas.dart
+++ lib/screens/obras_requisitadas.dart
@@ -7,13 +7,36 @@ class ObrasRequisitadas extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //? seta para voltar a página anterior
         iconTheme: const IconThemeData(
           color: Colors.black,
         ),
-        elevation: 0,
-        backgroundColor: Colors.white,
+        elevation: 0, //? retirar a sombra da barra do título
+        backgroundColor: Colors.white, //? Cor da barra do título
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(
+              30.0), //? distancia entre o título e a seta para voltar
+
+          //? Título
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: const [
+                Text(
+                  'Obras requisitadas',
+                  style: TextStyle(color: Colors.black, fontSize: 30.0),
+                ),
+              ],
+            ),
+          ),
+          //? _______________________________________________________________
+        ),
       ),
       backgroundColor: Colors.white,
+
+      //? DADOS DAS OBRAS REQUISITADAS
+
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(left: 20.0),
@@ -21,11 +44,6 @@ class ObrasRequisitadas extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const Text(
-                'Obras requisitadas',
-                style: TextStyle(fontSize: 25),
-              ),
-              const SizedBox(height: 34),
               SingleChildScrollView(
                 child: Column(
                   children: [
