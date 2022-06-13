1558508a94f5071a2b8bed7310a17075bafe50ee
Lodney Santos <a21505293@alunos.ulht.pt>
2022-06-13 17:16:41 +0100

    menu lateral design

---
+++ lib/screens/menu_lateral.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuLateral extends StatefulWidget {
+  const MenuLateral({Key? key}) : super(key: key);
+
+  @override
+  State<MenuLateral> createState() => _MenuLateralState();
+}
+
+class _MenuLateralState extends State<MenuLateral> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
