b89c110f79d58e8118ee0f951d5781e402f126c0
Lodney Santos <a21505293@alunos.ulht.pt>
2022-06-13 17:21:59 +0100

    ...

--- lib/screens/menu_lateral.dart
+++ lib/screens/menu_lateral.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
+
+class _MenuLateralState extends State<MenuLateral> {
+  @override
+  Widget build(BuildContext context) {
+    //
+    return HiddenDrawerMenu(
+      backgroundColorMenu: Colors.blueAccent,
+      screens: const [],
+    );
+  }
+
+  final List<ScreenHiddenDrawer> _screens = [];
+}
 
 class MenuLateral extends StatefulWidget {
   const MenuLateral({Key? key}) : super(key: key);
 
   @override
   State<MenuLateral> createState() => _MenuLateralState();
-}
-
-class _MenuLateralState extends State<MenuLateral> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
